b933de2115ff7449c3cf01af756ab45022cb6172
Erdem Tsynduev <erdemtsynduev@gmail.com>
2026-01-15 15:34:57 +0300

    feat: Up version library 0.4.3 -> 0.4.4

--- accelera/build.gradle.kts
+++ accelera/build.gradle.kts
@@ -76,7 +76,7 @@ afterEvaluate {
 
                 groupId = "com.github.cubesolutionsgit"
                 artifactId = "accelera"
-                version = "0.4.3"
+                version = "0.4.4"
             }
         }
     }
